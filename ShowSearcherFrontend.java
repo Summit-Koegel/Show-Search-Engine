5e708fc80c4a76fd15447c0d24d4f39b91cf9b66
summit <skoegel@wisc.edu>
2022-03-02 04:51:01 +0000

    Fixes all the output errors with ShowSearcherFrontend.java

--- ShowSearcherFrontend.java
+++ ShowSearcherFrontend.java
@@ -11,6 +11,12 @@ public class ShowSearcherFrontend implements IShowSearcherFrontend{
     private IShowSearcherBackend backend; // Instance of backend
     String input;
 
+    private char space1 = 'x';
+    private char space2 = 'x';
+    private char space3 = 'x';
+    private char space4 = 'x';
+
+
     /**
      * Constructor that uses backend instance and makes scanner
      * @param backend - Instance of backend
@@ -50,34 +56,39 @@ public class ShowSearcherFrontend implements IShowSearcherFrontend{
 
         message();
 
-        displayCommandMenu();
-
-        String choice = scan.nextLine();
-
         //Checks command from command menu and sends you to appropriate method
         while(count == 0){
-            if(choice.equals("1") || choice.toLowerCase().equals("t")){
+
+		displayCommandMenu();
+
+       		String choice1 = scan.nextLine();
+
+		if(choice1.equals("")){
+			choice1 = scan.nextLine();
+		}
+
+            if(choice1.equals("1") || choice1.toLowerCase().equals("t")){
                 System.out.print("Choose a word that you would like to search for: ");
                 titleSearch();
             }
     
-	    else if(choice.equals("2") || choice.toLowerCase().equals("y")){
+	    else if(choice1.equals("2") || choice1.toLowerCase().equals("y")){
                 System.out.print("Choose a year that you would like to search for: ");
                 yearSearch();
             }
     
-	    else if(choice.equals("3") || choice.toLowerCase().equals("f")){
+	    else if(choice1.equals("3") || choice1.toLowerCase().equals("f")){
                 toggleFilter();
             }
-    
-	    else if(choice.equals("4") || choice.toLowerCase().equals("q")){
+   
+	    else if(choice1.equals("4") || choice1.toLowerCase().equals("q")){
                 count++;
                 System.out.println();
             }
     
             else{
                 System.out.println("That is not a valid input.");
-                displayCommandMenu();
+		System.out.println("!" + choice1 + "!");	
             }
         }
         
@@ -101,24 +112,7 @@ public class ShowSearcherFrontend implements IShowSearcherFrontend{
 
     }
 
-    /**
-     * Toggles the "x" on and off for which provider you want to see
-     */
-    public void toggleFilter(){
-
-        int count = 0;
-
-        String space1 = null;
-        String space2 = null;
-        String space3 = null;
-        String space4 = null;
-
-        space1 = "x";
-        space2 = "x";
-        space3 = "x";
-        space4 = "x";
-        
-        
+    public void toggleHelper(){
 
         System.out.println("Providers that shows are being searched for:");
 
@@ -128,73 +122,91 @@ public class ShowSearcherFrontend implements IShowSearcherFrontend{
         System.out.println("4) _" + space4 + "_ [D]isney+");
         System.out.println("5) [Q]uit toggling provider filters");
 
-        System.out.println("Choose the provider that you'd like to toggle the filter for: ");
-
-	String choice = scan.nextLine();
-
-        //Sets provider on or off depending on user input
-        if(choice == "1" || choice.toLowerCase() == "n"){
-            space1 = "_";
-            //count++;
-            backend.setProviderFilter("Netflix", false);
-            toggleFilter();
-        }
-
-        if(choice == "2" || choice.toLowerCase() == "h"){
-            space2 = "_";
-            count++;
-            backend.setProviderFilter("Hulu", false);
-            toggleFilter();
-        }
-
-        if(choice == "3" || choice.toLowerCase() == "p"){
-            space3 = "_";
-            count++;
-            backend.setProviderFilter("Rrime Video", false);
-            toggleFilter();
-        }
-
-        if(choice == "4" || choice.toLowerCase() == "d"){
-            space4 = "_";
-            count++;
-            backend.setProviderFilter("Disney+", false);
-            toggleFilter();
-        }
-
-        if(choice == "5" || choice.toLowerCase() == "q"){
-            displayCommandMenu();
-        }
-        
+        System.out.print("Choose the provider that you'd like to toggle the filter for: ");
     }
 
     /**
+     * Toggles the "x" on and off for which provider you want to see
+     */
+    public void toggleFilter(){
+
+	toggleHelper();
+
+	String choice = scan.nextLine();
+	
+		
+   	//Sets provider on or off depending on user input
+        while(!(choice.equals("5")) && !(choice.toLowerCase().equals("q"))){
+            if(choice.equals("1") || choice.toLowerCase().equals("n")){
+                space1 = '_';
+                backend.setProviderFilter("Netflix", false);
+                toggleHelper();
+		choice = scan.nextLine();
+            }
+    
+            else if(choice.equals("2") || choice.toLowerCase().equals("h")){
+                space2 = '_';
+                backend.setProviderFilter("Hulu", false);
+                toggleHelper();
+		choice = scan.nextLine();
+            }
+    
+            else if(choice.equals("3") || choice.toLowerCase().equals("p")){
+                space3 = '_';
+                backend.setProviderFilter("Prime Video", false);
+                toggleHelper();
+		choice = scan.nextLine();
+            }
+    
+            else if(choice.equals("4") || choice.toLowerCase().equals("d")){
+                space4 = '_';
+                backend.setProviderFilter("Disney+", false);
+                toggleHelper();
+		choice = scan.nextLine();
+            }
+    
+            else if(choice.equals("5") || choice.toLowerCase().equals("q")){
+                break;
+            }
+
+            else{
+                System.out.println("That is not a valid input.");
+                toggleHelper();
+		choice = scan.nextLine();
+            }
+        }
+    }
+    /**
+	}	
      * Displays the formatted shows based on user input and each shows
      * various specifications
      */
     @Override
     public void displayShows(List<IShow> shows) {
 
-        System.out.println("Found " + backend.getNumberOfShows() + "/5368 matches.");
+        System.out.println("Found " + shows.size() + "/" + backend.getNumberOfShows() +  " matches.");
 
         for(int i = 0; i < shows.size(); i++){
             System.out.print((i+1) + ". " + shows.get(i).getTitle() + "\n\t" + shows.get(i).getRating()
              + "/100 (" + shows.get(i).getYear() + ") on: ");
 
              if((shows.get(i).isAvailableOn("Netflix"))){
-                System.out.println("Netflix ");
+                System.out.print("Netflix ");
             }
     
             if(shows.get(i).isAvailableOn("Hulu")){
-                System.out.println("Hulu ");
+                System.out.print("Hulu ");
             }
     
             if(shows.get(i).isAvailableOn("Prime Video")){
-                System.out.println("Prime Video ");
+                System.out.print("Prime Video ");
             }
     
             if(shows.get(i).isAvailableOn("Disney+")){
-                System.out.println("Disney+ ");
+                System.out.print("Disney+ ");
             }
+
+	    System.out.println();
         }
         
     }
